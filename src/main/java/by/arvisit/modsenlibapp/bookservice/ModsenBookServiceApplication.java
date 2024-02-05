1a224eedba70fe9b3109c26df81444c3ffd6f0fa
Artem Sitko <sitko.ar@gmail.com>
2024-02-05 15:18:34 +0300

    Init maven project

---
+++ src/main/java/by/arvisit/modsenlibapp/bookservice/ModsenBookServiceApplication.java
@@ -0,0 +1,13 @@
+package by.arvisit.modsenlibapp.bookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModsenBookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModsenBookServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/arvisit/modsenlibapp/bookservice/ModsenBookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.arvisit.modsenlibapp.bookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModsenBookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
