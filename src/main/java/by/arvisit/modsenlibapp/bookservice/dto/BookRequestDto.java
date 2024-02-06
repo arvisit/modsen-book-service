5c623d5a71c82ff0a61937195860910801ca1f9a
Artem Sitko <sitko.ar@gmail.com>
2024-02-06 21:08:06 +0300

    Correct mistakes in messages

--- src/main/java/by/arvisit/modsenlibapp/bookservice/dto/BookRequestDto.java
+++ src/main/java/by/arvisit/modsenlibapp/bookservice/dto/BookRequestDto.java
@@ -13,17 +13,17 @@ public record BookRequestDto(
         @NotBlank(message = "Book isbn must be not null nor blank.")
         @Isbn(message = "Book isbn should rather consist of 10 digits and 3 hyphens or 13 digits and 4 hyphens. Also the last character could be X. No hyphens concatenated. No hyphens at the beginnig and end.")
         String isbn,
-        @Length(max = 50, message = "Book title length should be not greater then {max}")
+        @Length(max = 50, message = "Book title length should be not greater than {max}")
         @NotBlank(message = "Book title must be not null nor blank.")
         String title,
         @NotNull(message = "Book genre must be not null.")
         @IsGenreExist(message = "Known genre should be used.")
         GenreDto genre,
         @NotBlank(message = "Book description must be not null nor blank.")
-        @Length(max = 255, message = "Book description length should be not greater then {max}")
+        @Length(max = 255, message = "Book description length should be not greater than {max}")
         String description,
         @NotBlank(message = "Book author must be not null nor blank.")
-        @Length(max = 50, message = "Book description length should be not greater then {max}")
+        @Length(max = 50, message = "Book description length should be not greater than {max}")
         String author) {
 
 }
