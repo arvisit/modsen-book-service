bea8b5bce4042dba1b17023a534c7cdd8d69c8db
Artem Sitko <sitko.ar@gmail.com>
2024-02-07 21:15:18 +0300

    Extract @ValueSource to methods

--- src/test/java/by/arvisit/modsenlibapp/bookservice/controller/BookControllerTest.java
+++ src/test/java/by/arvisit/modsenlibapp/bookservice/controller/BookControllerTest.java
@@ -6,11 +6,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.stream.Stream;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -37,6 +39,7 @@ class BookControllerTest {
 
     private static final String INVALID_ISBN_MESSAGE = "Book isbn should rather consist of 10 digits and 3 hyphens or 13 digits and 4 hyphens. Also the last character could be X. No hyphens concatenated. No hyphens at the beginnig and end.";
     private static final String GENRE_NOT_EXIST_MESSAGE = "Known genre should be used.";
+    private static final String INVALID_UUID_MESSAGE = "must be a valid UUID";
 
     @Autowired
     private MockMvc mockMvc;
@@ -65,52 +68,52 @@ class BookControllerTest {
                     .andDo(print())
                     .andExpect(status().isCreated());
         }
-        
+
         @Test
         void shouldMapsToBusinessModel_when_passValidInput() throws Exception {
             BookRequestDto requestDto = BookTestData.getDefaultBookRequestDto().build();
             BookResponseDto responseDto = BookTestData.getDefaultBookResponseDto().build();
-            
+
             Mockito.when(genreService.isGenreExists(Mockito.any())).thenReturn(true);
             Mockito.when(bookService.save(requestDto)).thenReturn(responseDto);
-            
-            MvcResult mvcResult = mockMvc.perform(post(BookTestData.URL_BOOKS_ENDPOINT)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(requestDto)))
-            .andDo(print())
-            .andExpect(status().isCreated())
-            .andReturn();
-            
-            Mockito.verify(bookService, Mockito.times(1)).save(requestDto);
-            String actualResponseBody = mvcResult.getResponse().getContentAsString();
-            BookResponseDto result = objectMapper.readValue(actualResponseBody, BookResponseDto.class);
-            
-            Assertions.assertThat(result).isEqualTo(responseDto);
-        }
-        
-        @Test
-        void shouldReturnValidBook_when_passValidInput() throws Exception {
-            BookRequestDto requestDto = BookTestData.getDefaultBookRequestDto().build();
-            BookResponseDto responseDto = BookTestData.getDefaultBookResponseDto().build();
-            
-            Mockito.when(genreService.isGenreExists(Mockito.any())).thenReturn(true);
-            Mockito.when(bookService.save(requestDto)).thenReturn(responseDto);
-            
+
             MvcResult mvcResult = mockMvc.perform(post(BookTestData.URL_BOOKS_ENDPOINT)
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(objectMapper.writeValueAsString(requestDto)))
                     .andDo(print())
                     .andExpect(status().isCreated())
                     .andReturn();
-            
+
+            Mockito.verify(bookService, Mockito.times(1)).save(requestDto);
             String actualResponseBody = mvcResult.getResponse().getContentAsString();
-            
-            Assertions.assertThat(actualResponseBody).isEqualToIgnoringWhitespace(objectMapper.writeValueAsString(responseDto));
+            BookResponseDto result = objectMapper.readValue(actualResponseBody, BookResponseDto.class);
+
+            Assertions.assertThat(result).isEqualTo(responseDto);
+        }
+
+        @Test
+        void shouldReturnValidBook_when_passValidInput() throws Exception {
+            BookRequestDto requestDto = BookTestData.getDefaultBookRequestDto().build();
+            BookResponseDto responseDto = BookTestData.getDefaultBookResponseDto().build();
+
+            Mockito.when(genreService.isGenreExists(Mockito.any())).thenReturn(true);
+            Mockito.when(bookService.save(requestDto)).thenReturn(responseDto);
+
+            MvcResult mvcResult = mockMvc.perform(post(BookTestData.URL_BOOKS_ENDPOINT)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(requestDto)))
+                    .andDo(print())
+                    .andExpect(status().isCreated())
+                    .andReturn();
+
+            String actualResponseBody = mvcResult.getResponse().getContentAsString();
+
+            Assertions.assertThat(actualResponseBody)
+                    .isEqualToIgnoringWhitespace(objectMapper.writeValueAsString(responseDto));
         }
 
         @ParameterizedTest
-        @ValueSource(strings = { "-0123-456-789", "---0123456789", "0123-456-789-", "012-345--678-9", "01-23-4567X-9",
-                "1-2-3-3", "012-3456-789-X" })
+        @MethodSource("by.arvisit.modsenlibapp.bookservice.controller.BookControllerTest#invalidTenDigitIsbn")
         void shouldReturn400_when_passInvalidTenDigitIsbn(String isbn) throws Exception {
             BookRequestDto requestDto = BookTestData.getDefaultBookRequestDto()
                     .withIsbn(isbn)
@@ -129,8 +132,7 @@ class BookControllerTest {
         }
 
         @ParameterizedTest
-        @ValueSource(strings = { "-0123-4561-7812-9", "---0123456789012", "01231-4562-7893-", "0121-3452--6782-9",
-                "011-2322-4567X-9", "1-2-3-3-4", "012-345-678-9012-1" })
+        @MethodSource("by.arvisit.modsenlibapp.bookservice.controller.BookControllerTest#invalidThirteenDigitIsbn")
         void shouldReturn400_when_passInvalidThirteenDigitIsbn(String isbn) throws Exception {
             BookRequestDto requestDto = BookTestData.getDefaultBookRequestDto()
                     .withIsbn(isbn)
@@ -163,5 +165,15 @@ class BookControllerTest {
                     .andExpect(status().isBadRequest())
                     .andExpect(content().string(containsString(expectedContent)));
         }
+    }
+
+    static Stream<String> invalidTenDigitIsbn() {
+        return Stream.of("-0123-456-789", "---0123456789", "0123-456-789-", "012-345--678-9", "01-23-4567X-9",
+                "1-2-3-3", "012-3456-789-X");
+    }
+
+    static Stream<String> invalidThirteenDigitIsbn() {
+        return Stream.of("-0123-4561-7812-9", "---0123456789012", "01231-4562-7893-", "0121-3452--6782-9",
+                "011-2322-4567X-9", "1-2-3-3-4", "012-345-678-9012-1");
     }
 }
