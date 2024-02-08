9c706291a24ed8d51ac0a0323d445bfc72ac22e3
arvisit <95239665+arvisit@users.noreply.github.com>
2024-02-08 23:12:29 +0300

    feature/03 add discovery server (#3)
    * Enable eureka client
    
    * Modify path to changelogs
    
    * Disable config-server/eureka-client for tests

--- src/main/java/by/arvisit/modsenlibapp/bookservice/ModsenBookServiceApplication.java
+++ src/main/java/by/arvisit/modsenlibapp/bookservice/ModsenBookServiceApplication.java
@@ -2,12 +2,14 @@ package by.arvisit.modsenlibapp.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ModsenBookServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ModsenBookServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ModsenBookServiceApplication.class, args);
+    }
 
 }
