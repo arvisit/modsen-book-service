19b961c6de46bcb0cf5cce00d300b8897c573485
Artem Sitko <sitko.ar@gmail.com>
2024-02-13 04:33:16 +0300

    Add save a new book in the library service

---
+++ src/main/java/by/arvisit/modsenlibapp/bookservice/client/DiscoveryLibraryClient.java
@@ -0,0 +1,18 @@
+package by.arvisit.modsenlibapp.bookservice.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import by.arvisit.modsenlibapp.bookservice.dto.LibraryBookDto;
+import by.arvisit.modsenlibapp.innerfilterstarter.client.InnerCommunicationFeignConfiguration;
+
+@Profile({ "docker" })
+@FeignClient(value = "modsen-library-service", configuration = InnerCommunicationFeignConfiguration.class)
+public interface DiscoveryLibraryClient extends LibraryClient {
+
+    @Override
+    @PostMapping("/api/v1/books")
+    LibraryBookDto addNewBook(@RequestBody LibraryBookDto newBook);
+}
