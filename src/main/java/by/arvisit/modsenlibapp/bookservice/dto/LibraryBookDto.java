3817bfce5947e314a70b21e45aed50e6c4e429f9
Artem Sitko <sitko.ar@gmail.com>
2024-02-14 03:55:28 +0300

    Implement swagger docs

--- src/main/java/by/arvisit/modsenlibapp/bookservice/dto/LibraryBookDto.java
+++ src/main/java/by/arvisit/modsenlibapp/bookservice/dto/LibraryBookDto.java
@@ -2,9 +2,11 @@ package by.arvisit.modsenlibapp.bookservice.dto;
 
 import org.hibernate.validator.constraints.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder(setterPrefix = "with")
-public record LibraryBookDto(@UUID String id) {
-
+public record LibraryBookDto(@UUID
+        @Schema(description = "Book id", example = "d4cee2e6-cb52-4a17-ae47-f4ad85dd8b86")
+        String id) {
 }
